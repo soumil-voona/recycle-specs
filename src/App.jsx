04acaf058416433afa3d2d1cd704873044e1dbbb
Soumil Voona <voonasoumil@gmail.com>
2026-04-01 17:56:36 -0500

    remove easter

--- src/App.jsx
+++ src/App.jsx
@@ -273,7 +273,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/easter" element={<Easter />} />
+        {/* <Route path="/easter" element={<Easter />} /> */}
         <Route 
           path="/volunteers" 
           element={
